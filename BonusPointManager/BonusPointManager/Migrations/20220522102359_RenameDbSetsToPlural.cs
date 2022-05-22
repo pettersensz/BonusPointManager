2b0d7b286b971810f3e27bebf16ce4a62e592202
Sigmund Pettersen <sigmund.pettersen@cegal.com>
2022-05-22 12:24:55 +0200

    Add migration for renaming dbsets to plural

---
+++ BonusPointManager/BonusPointManager/Migrations/20220522102359_RenameDbSetsToPlural.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BonusPointManager.Migrations
+{
+    public partial class RenameDbSetsToPlural : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EurobonusTransaction",
+                table: "EurobonusTransaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EurobonusStatusLevel",
+                table: "EurobonusStatusLevel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EurobonusPointType",
+                table: "EurobonusPointType");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EurobonusAccount",
+                table: "EurobonusAccount");
+
+            migrationBuilder.RenameTable(
+                name: "EurobonusTransaction",
+                newName: "EurobonusTransactions");
+
+            migrationBuilder.RenameTable(
+                name: "EurobonusStatusLevel",
+                newName: "EurobonusStatusLevels");
+
+            migrationBuilder.RenameTable(
+                name: "EurobonusPointType",
+                newName: "EurobonusPointTypes");
+
+            migrationBuilder.RenameTable(
+                name: "EurobonusAccount",
+                newName: "EurobonusAccounts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EurobonusTransactions",
+                table: "EurobonusTransactions",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EurobonusStatusLevels",
+                table: "EurobonusStatusLevels",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EurobonusPointTypes",
+                table: "EurobonusPointTypes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EurobonusAccounts",
+                table: "EurobonusAccounts",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EurobonusTransactions",
+                table: "EurobonusTransactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EurobonusStatusLevels",
+                table: "EurobonusStatusLevels");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EurobonusPointTypes",
+                table: "EurobonusPointTypes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EurobonusAccounts",
+                table: "EurobonusAccounts");
+
+            migrationBuilder.RenameTable(
+                name: "EurobonusTransactions",
+                newName: "EurobonusTransaction");
+
+            migrationBuilder.RenameTable(
+                name: "EurobonusStatusLevels",
+                newName: "EurobonusStatusLevel");
+
+            migrationBuilder.RenameTable(
+                name: "EurobonusPointTypes",
+                newName: "EurobonusPointType");
+
+            migrationBuilder.RenameTable(
+                name: "EurobonusAccounts",
+                newName: "EurobonusAccount");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EurobonusTransaction",
+                table: "EurobonusTransaction",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EurobonusStatusLevel",
+                table: "EurobonusStatusLevel",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EurobonusPointType",
+                table: "EurobonusPointType",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EurobonusAccount",
+                table: "EurobonusAccount",
+                column: "Id");
+        }
+    }
+}
