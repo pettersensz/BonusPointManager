9788426abdd738baa99741f9cdac238a4c27edab
Sigmund Pettersen <sigmund.pettersen@cegal.com>
2022-05-23 21:09:06 +0200

    Back and forth with migrations

---
+++ BonusPointManager/BonusPointManager/Migrations/20220523190215_LinkTransactionsToAccount.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BonusPointManager.Migrations
+{
+    public partial class LinkTransactionsToAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EurobonusAccountId",
+                table: "EurobonusTransactions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EurobonusTransactions_EurobonusAccountId",
+                table: "EurobonusTransactions",
+                column: "EurobonusAccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EurobonusTransactions_EurobonusAccounts_EurobonusAccountId",
+                table: "EurobonusTransactions",
+                column: "EurobonusAccountId",
+                principalTable: "EurobonusAccounts",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EurobonusTransactions_EurobonusAccounts_EurobonusAccountId",
+                table: "EurobonusTransactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EurobonusTransactions_EurobonusAccountId",
+                table: "EurobonusTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "EurobonusAccountId",
+                table: "EurobonusTransactions");
+        }
+    }
+}
