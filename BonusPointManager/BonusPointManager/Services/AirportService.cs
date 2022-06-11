e0b0a600640128f103e905816247aab94cb1550c
Sigmund Pettersen <sigmund.pettersen@cegal.com>
2022-06-11 13:49:18 +0200

    Refactor AirportService

--- BonusPointManager/BonusPointManager/Services/AirportService.cs
+++ BonusPointManager/BonusPointManager/Services/AirportService.cs
@@ -7,12 +7,14 @@ namespace BonusPointManager.Services
   public class AirportService
   {
     // TODO maybe move to appsettings file to make it replaceable
-    string PathToAirportFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Data\airports.csv";
+    readonly string _pathToAirportFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Data\airports.csv";
+    readonly NumberStyles _numberStyle = NumberStyles.AllowDecimalPoint;
+    readonly CultureInfo _culture = CultureInfo.CreateSpecificCulture("en-US");
 
     public bool AirportExistsInFileIcao(string IcaoCode)
     {
       IcaoCode = IcaoCode.ToUpper();
-      using var reader = new StreamReader(PathToAirportFile);
+      using var reader = new StreamReader(_pathToAirportFile);
       var line = reader.ReadLine();
       while (line != null)
       {
@@ -33,7 +35,7 @@ namespace BonusPointManager.Services
 
       var airport = new Airport();
 
-      using var reader = new StreamReader(PathToAirportFile);
+      using var reader = new StreamReader(_pathToAirportFile);
       var line = reader.ReadLine();
       while (line != null)
       {
@@ -53,26 +55,20 @@ namespace BonusPointManager.Services
     private Airport ReadAirportDataFromStringArray(string[] airportDetails)
     {
       var airport = new Airport();
-      // TODO All strings must be trimmed for quotes ""
+      
       airport.IcaoCode = airportDetails[1].Trim('"');
       airport.Name = airportDetails[3].Trim('"');
 
-      // Nedd to take into account that airport name can have a comma in it
       var add = 0;
-      var style = NumberStyles.AllowDecimalPoint;
-      var culture = CultureInfo.CreateSpecificCulture("en-US");
-      decimal latitudeDeg;
-      var commaFreeName = Decimal.TryParse(airportDetails[4], style, culture, out latitudeDeg);
 
-      if (!commaFreeName)
+      if (AirportNameContainsComma(airportDetails[4]))
       {
         airport.Name = airportDetails[3].Trim('"') + "," + airportDetails[4].Trim('"');
         add++;
       }
 
-      // TODO decimals get read with all digits, but saved to DB with only two digits. Why?
-      airport.LatitudeDeg = Decimal.Parse(airportDetails[4 + add], style, culture);
-      airport.LongitudeDeg = Decimal.Parse(airportDetails[5 + add], style, culture);
+      airport.LatitudeDeg = Decimal.Parse(airportDetails[4 + add], _numberStyle, _culture);
+      airport.LongitudeDeg = Decimal.Parse(airportDetails[5 + add], _numberStyle, _culture);
 
       airport.ElevationFt = Int32.Parse(airportDetails[6 + add]);
 
@@ -83,16 +79,28 @@ namespace BonusPointManager.Services
       return airport;
     }
 
+    private bool AirportNameContainsComma(string nextItemInArray)
+    {
+      return !Decimal.TryParse(nextItemInArray, _numberStyle, _culture, out _);
+    }
+
     public bool AirportExistsInFileIata(string IataCode)
     {
       IataCode = IataCode.ToUpper();
-      using var reader = new StreamReader(PathToAirportFile);
+      using var reader = new StreamReader(_pathToAirportFile);
       var line = reader.ReadLine();
       while (line != null)
       {
         var airportDetails = line.Split(',');
-        // TODO deal with airports with , in name, will offsett the index
-        var airportIataCode = airportDetails[13].Trim('"');
+        string airportIataCode;
+        if (AirportNameContainsComma(airportDetails[4]))
+        {
+          airportIataCode = airportDetails[14].Trim('"');
+        }
+        else
+        {
+          airportIataCode = airportDetails[13].Trim('"');
+        }
         if (airportIataCode == IataCode)
         {
           return true;
